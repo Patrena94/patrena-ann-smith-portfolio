69a6aeeb7f08d42ed02cca2e97d545d8f48d520e
Vashti <patrena@sbcglobal.net>
2021-10-10 07:22:40 -0700

    updated carousel to include deployed links, github page, and github repository. Also added sixth project

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -7,6 +7,7 @@ import Scheduler from "./../../assets/carousel/Scheduler.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import portfoliohtmlcss from "./../../assets/carousel/portfoliohtmlcss.png";
 
 class BootstrapCarouselComponent extends React.Component {
   render() {
@@ -33,7 +34,8 @@ class BootstrapCarouselComponent extends React.Component {
                     alt="Budget Tracker"
                   />
                   <Carousel.Caption>
-                    <h3 className="title"
+                    <h3
+                      className="title"
                       style={{
                         color: "gold",
                         display: "flex",
@@ -42,19 +44,34 @@ class BootstrapCarouselComponent extends React.Component {
                         text: "font-weight-italic",
                         fontSize: "36px",
                       }}
-                      >
-                    P3S Corp Budget Tracker</h3>
-                    <p  className="overview"
+                    >
+                      P3S Corp Budget Tracker
+                    </h3>
+                    <p
+                      className="overview"
                       style={{
                         color: "#C49E93",
-                        display: "flex",
-                        justifyContent: "center",
                         textAlign: "center",
-                        text: "font-weight-italic",
+                        text: "bold",
                         fontSize: "24px",
                       }}
                     >
-                     PS Corp’s back end for your e-commerce site. The application will provide owners with vital statistic on stock.  This will allow for owners to determine revenue flow; popularity of item; as well as make adjustment in terms of marketing based on sales.  The application tracks product, stock, and by Category. The application also allows for owners to easily updates Tag, categories, and products.  
+                      PS Corp’s back end for your e-commerce site. The
+                      application will provide owners with vital statistic on
+                      stock. This will allow for owners to determine revenue
+                      flow; popularity of item; as well as make adjustment in
+                      terms of marketing based on sales. The application tracks
+                      product, stock, and by Category. The application also
+                      allows for owners to easily updates Tag, categories, and
+                      products. You may review the
+                      <a href="https://p3s-corp-budget-tracker.herokuapp.com/">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/PS-Corp-Budget-Tracker">
+                        github respository
+                      </a>
+                      .
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
@@ -70,7 +87,8 @@ class BootstrapCarouselComponent extends React.Component {
                   />
 
                   <Carousel.Caption>
-                    <h3 className="title"
+                    <h3
+                      className="title"
                       style={{
                         color: "gold",
                         display: "flex",
@@ -79,15 +97,15 @@ class BootstrapCarouselComponent extends React.Component {
                         text: "font-weight-italic",
                         fontSize: "36px",
                       }}
-                    >The Casual Oberver</h3>
+                    >
+                      The Casual Oberver
+                    </h3>
                     <p
                       className="overview"
                       style={{
                         color: "#914875",
-                        display: "flex",
-                        justifyContent: "center",
                         textAlign: "center",
-                        text: "font-weight-italic",
+                        text: "bold",
                         fontSize: "24px",
                       }}
                     >
@@ -95,7 +113,14 @@ class BootstrapCarouselComponent extends React.Component {
                       The Casual Observer is a blog site that enable the
                       knowledge thirsty individual to publish articles blog,
                       posts, thoughts and opinions; as well as review the
-                      aforementioned by other members.
+                      aforementioned by other members. You may review the
+                      <a href="https://the-casual-observer.herokuapp.com/">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/the-casual-observer">
+                        github respository
+                      </a>
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
@@ -110,7 +135,8 @@ class BootstrapCarouselComponent extends React.Component {
                     alt="Note Taker Express"
                   />
                   <Carousel.Caption>
-                    <h3 className="title"
+                    <h3
+                      className="title"
                       style={{
                         color: "gold",
                         display: "flex",
@@ -119,18 +145,30 @@ class BootstrapCarouselComponent extends React.Component {
                         text: "font-weight-italic",
                         fontSize: "36px",
                       }}
-                      > Note Taker Express</h3>
-                    <p  className="overview"
+                    >
+                      {" "}
+                      Note Taker Express
+                    </h3>
+                    <p
+                      className="overview"
                       style={{
                         color: "#C49E93",
-                        display: "flex",
-                        justifyContent: "center",
                         textAlign: "center",
-                        text: "font-weight-italic",
+                        text: "bold",
                         fontSize: "24px",
                       }}
                     >
-                     The note taker express allows for user to take effective notes and organize their schedule in a few simple steps.  The user has the ability to create, review, update, and delete notes.
+                      The note taker express allows for user to take effective
+                      notes and organize their schedule in a few simple steps.
+                      The user has the ability to create, review, update, and
+                      delete notes. You may review the
+                      <a href="https://shrouded-brook-35525.herokuapp.com/notes">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/Note-Taker-Express">
+                        github respository
+                      </a>
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
@@ -145,7 +183,8 @@ class BootstrapCarouselComponent extends React.Component {
                     alt="Stack_Underflo"
                   />
                   <Carousel.Caption>
-                    <h3 className="title"
+                    <h3
+                      className="title"
                       style={{
                         color: "gold",
                         display: "flex",
@@ -154,18 +193,31 @@ class BootstrapCarouselComponent extends React.Component {
                         text: "font-weight-italic",
                         fontSize: "36px",
                       }}
-                      >Stack_Underflo</h3>
-                    <p  className="overview"
+                    >
+                      Stack_Underflo
+                    </h3>
+                    <p
+                      className="overview"
                       style={{
                         color: "#914875",
-                        display: "flex",
-                        justifyContent: "center",
                         textAlign: "center",
-                        text: "font-weight-italic",
+                        text: "bold",
                         fontSize: "24px",
                       }}
                     >
-                      The Stack-Underflow app is a diversified resource center where fledgling coders go to expand their knowledge to the next level. It provides coders with resources to ignite their light about a subject matter.  The application provides references to websites, books, coding puzzles, and answer to questions submitted.
+                      The Stack-Underflow app is a diversified resource center
+                      where fledgling coders go to expand their knowledge to the
+                      next level. It provides coders with resources to ignite
+                      their light about a subject matter. The application
+                      provides references to websites, books, coding puzzles,
+                      and answer to questions submitted. You may review the{" "}
+                      <a href="https://stack-underflo.herokuapp.com/">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/stack-underflow">
+                        github respository
+                      </a>
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
@@ -180,7 +232,8 @@ class BootstrapCarouselComponent extends React.Component {
                     alt="Stack_Underflo"
                   />
                   <Carousel.Caption>
-                    <h3 className="title"
+                    <h3
+                      className="title"
                       style={{
                         color: "gold",
                         display: "flex",
@@ -189,18 +242,75 @@ class BootstrapCarouselComponent extends React.Component {
                         text: "font-weight-italic",
                         fontSize: "36px",
                       }}
-                      >Smith Corporation Work Day Scheduler</h3>
-                    <p  className="overview"
+                    >
+                      Smith Corporation Work Day Scheduler
+                    </h3>
+                    <p
+                      className="overview"
                       style={{
                         color: "#C49E93",
-                        display: "flex",
-                        justifyContent: "center",
                         textAlign: "center",
-                        text: "font-weight-italic",
+                        text: "bold",
                         fontSize: "24px",
                       }}
                     >
-                      The Stack-Underflow app is a diversified resource center where fledgling coders go to expand their knowledge to the next level. It provides coders with resources to ignite their light about a subject matter.  The application provides references to websites, books, coding puzzles, and answer to questions submitted.
+                      The work day schedule allows employees to maintain
+                      organization by scheduling their day in advance. The
+                      schedule will allow for employees to determine, if, their
+                      tasks are being met on time. It is a great way to measure
+                      employee productivity. You may review the{" "}
+                      <a href="https://patrena94.github.io/Smith-Corporation-Work-Scheduler/">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/Smith-Corporation-Work-Scheduler">
+                        github respository
+                      </a>
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item Interval={2000}>
+                  <img
+                    className="d-block w-100"
+                    src={portfoliohtmlcss}
+                    width="800"
+                    height="800"
+                    text="Stack_Underflo"
+                    alt="Stack_Underflo"
+                  />
+                  <Carousel.Caption>
+                    <h3
+                      className="title"
+                      style={{
+                        color: "gold",
+                        display: "flex",
+                        justifyContent: "center",
+                        textAlign: "center",
+                        text: "bold",
+                        fontSize: "36px",
+                      }}
+                    >
+                      Patrena A. D. Smith Portfolio
+                    </h3>
+                    <p
+                      className="overview"
+                      style={{
+                        color: "#C49E93",
+                        textAlign: "center",
+                        text: "bold",
+                        fontSize: "24px",
+                      }}
+                    >
+                      The Portfolio provides an employer with an high level
+                      overview of my professional work experience and body of
+                      work as a Coding student. You may review the{" "}
+                      <a href="https://patrena94.github.io/Patrena-A.-D.-Smith/">
+                        deployed link
+                      </a>{" "}
+                      and
+                      <a href="https://github.com/Patrena94/Patrena-A.-D.-Smith">
+                        github respository
+                      </a>
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
